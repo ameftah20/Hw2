dd53d23d5764e6d5cd957260c22db89cfb305af7
ameftah20 <ameftah@asu.edu>
2025-10-15 10:45:15 -0700

    Delete TP1 directory

--- TP1/TP1 Source Code/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module FoundationsF25 {
-    requires javafx.controls;
-    requires java.sql;
-    requires javafx.graphics; 
-    requires org.junit.jupiter.api;
-
-    opens applicationMain to javafx.graphics, javafx.fxml;
-}
